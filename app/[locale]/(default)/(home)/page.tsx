be8cd30b37319fbe155bb36942dc84659e277296
faikit <huijie.wang@hotmail.com>
2025-12-05 16:17:09 +0800

    .

--- app/[locale]/(default)/(home)/page.tsx
+++ app/[locale]/(default)/(home)/page.tsx
@@ -76,7 +76,7 @@ export default function Page(){
                 {isDesktop ? (
                     <div className="video-box">
                         <video autoPlay muted loop playsInline>
-                            <source src="https://assets.kinetixai.tech/kinetixai/index/banner.mp4" type="video/mp4" />
+                            <source src="https://assets.kinetixai.cn/AMS/201247.mp4" type="video/mp4" />
                         </video>
                     </div>
                 ) : (
