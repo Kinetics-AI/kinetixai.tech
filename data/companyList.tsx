df3dbc5d81bc5e1df6773f62954875bdbe484c8d
faikit <huijie.wang@hotmail.com>
2025-12-26 12:33:25 +0800

    .

--- data/companyList.tsx
+++ data/companyList.tsx
@@ -17,11 +17,11 @@ export const companyListData: LanguageData = {
             title: "自主设计兼量产医用上肢外骨骼",
             pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-1.png"
         },
-        {
-            year: "2023",
-            title: "全球首个端到端自动驾驶大模型</br>联合创始人HY说：“这是CVPR 30年史上第一篇自动驾驶最佳论文。”",
-            pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-2.png"
-        },
+        // {
+        //     year: "2023",
+        //     title: "全球首个端到端自动驾驶大模型</br>联合创始人HY说：“这是CVPR 30年史上第一篇自动驾驶最佳论文。”",
+        //     pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-2.png"
+        // },
         {
             year: "2024",
             title: "超百台L4自动驾驶矿卡商业化落地",
@@ -42,11 +42,11 @@ export const companyListData: LanguageData = {
             title: "全球首个开源分层AI Agent框架",
             pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-6.png"
         },
-        {
-            year: "2025",
-            title: "性能超过PI0.5的跨本体VLA大模型",
-            pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-7.png"
-        }
+        // {
+        //     year: "2025",
+        //     title: "性能超过PI0.5的跨本体VLA大模型",
+        //     pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-7.png"
+        // }
     ],
     en: [
         {
@@ -54,11 +54,11 @@ export const companyListData: LanguageData = {
             title: "Self-Designed and Mass-Produced Medical Upper-Limb Exoskeleton",
             pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-1.png"
         },
-        {
-            year: "2023",
-            title: "World’s First End-to-End Autonomous Driving Foundation Model</br>Co-founder HY stated, “This is the first Best Paper on Autonomous Driving in the 30-year history of CVPR.”",
-            pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-2.png"
-        },
+        // {
+        //     year: "2023",
+        //     title: "World’s First End-to-End Autonomous Driving Foundation Model</br>Co-founder HY stated, “This is the first Best Paper on Autonomous Driving in the 30-year history of CVPR.”",
+        //     pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-2.png"
+        // },
         {
             year: "2024",
             title: "Commercialized over 100 L4 autonomous mining trucks—ushering in a new era of intelligent automation in heavy industry.",
@@ -79,10 +79,10 @@ export const companyListData: LanguageData = {
             title: "The world's first hierarchical AI agent ranked first among open-source models on the GAIA benchmark and received more than 18k GitHub stars.",
             pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-6.png"
         },
-        {
-            year: "2025",
-            title: "Unified Vision-Language-Action (VLA) foundation model outperforming PI0.5",
-            pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-7.png"
-        }
+        // {
+        //     year: "2025",
+        //     title: "Unified Vision-Language-Action (VLA) foundation model outperforming PI0.5",
+        //     pic: "https://assets.kinetixai.cn/kinetixai/company/img-2-7.png"
+        // }
     ]
 };
